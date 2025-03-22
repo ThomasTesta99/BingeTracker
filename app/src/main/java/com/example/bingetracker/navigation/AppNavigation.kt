f7ab76e646b040c11aa644fc5f5299b073fe18ff
ThomasTesta99 <ttesta99@yahoo.com>
2025-03-22 18:13:43 -0400

    starting signin

--- app/src/main/java/com/example/bingetracker/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/bingetracker/navigation/AppNavigation.kt
@@ -4,12 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.bingetracker.pages.HomeScreen
 import com.example.bingetracker.pages.auth.SignUpScreen
 
 @Composable
 fun AppNavigation(navController: NavHostController){
     NavHost(navController, startDestination = "signup"){
-        composable("signup") { SignUpScreen {navController.navigate("home")}}
-        composable("home") {  }
+        composable("signup") { SignUpScreen {navController.navigate("home")} }
+        composable("home") { HomeScreen() }
     }
 }
---
+++ app/src/main/java/com/example/bingetracker/pages/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.bingetracker.pages
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(){
+    Text("Home")
+}
