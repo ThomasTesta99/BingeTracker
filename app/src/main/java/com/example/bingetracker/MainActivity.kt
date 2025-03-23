c8583139b84f57d5b819fac952ce4e1059de936e
ThomasTesta99 <ttesta99@yahoo.com>
2025-03-23 14:54:30 -0400

    merged sign n and sign up

--- app/src/main/java/com/example/bingetracker/MainActivity.kt
+++ app/src/main/java/com/example/bingetracker/MainActivity.kt
@@ -4,16 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.bingetracker.navigation.AppNavigation
-import com.example.bingetracker.ui.theme.BingeTrackerTheme
 import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/bingetracker/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/bingetracker/navigation/AppNavigation.kt
@@ -5,12 +5,12 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.bingetracker.pages.HomeScreen
-import com.example.bingetracker.pages.auth.SignUpScreen
+import com.example.bingetracker.pages.auth.AuthScreen
 
 @Composable
 fun AppNavigation(navController: NavHostController){
-    NavHost(navController, startDestination = "signup"){
-        composable("signup") { SignUpScreen {navController.navigate("home")} }
+    NavHost(navController, startDestination = "auth"){
+        composable("auth") { AuthScreen {navController.navigate("home")} }
         composable("home") { HomeScreen() }
     }
 }
