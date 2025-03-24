ee1ecc7eb29caf789a269b933a66cf1c8f3d0cc6
ThomasTesta99 <ttesta99@yahoo.com>
2025-03-24 16:31:13 -0400

    fixed HomeScreen

--- app/src/main/java/com/example/bingetracker/pages/HomeScreen.kt
+++ app/src/main/java/com/example/bingetracker/pages/HomeScreen.kt
@@ -1,12 +1,16 @@
 package com.example.bingetracker.pages
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.bingetracker.models.AuthModel
 
@@ -25,13 +29,20 @@ fun HomeScreen(navController: NavHostController, authModel: AuthModel) {
     }
 
     Column {
-        if (user != null) {
-            Text(text = "Welcome, ${user?.name}")
-            Button(onClick = { authModel.logout() }) {
-                Text("Logout")
+        when {
+            user == null && currentUserAuth != null -> {
+                Text("Loading...")
             }
-        } else {
-            Text(text = "Not logged in")
+            user != null -> {
+                Spacer(modifier = Modifier.height(100.dp))
+                Text(text = "Welcome, ${user?.name}")
+                Button(onClick = { authModel.logout() }) {
+                    Text("Logout")
+                }
+            }
+            else -> {
+                Text(text = "Not logged in")
+            }
         }
     }
 }
