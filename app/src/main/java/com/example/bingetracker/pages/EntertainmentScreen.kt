0f7f81bfb9cd14918c584652466c1621abdb47af
ThomasTesta99 <ttesta99@yahoo.com>
2025-05-05 12:10:57 -0400

    Fixed create card being blocked by keyboard

--- app/src/main/java/com/example/bingetracker/pages/EntertainmentScreen.kt
+++ app/src/main/java/com/example/bingetracker/pages/EntertainmentScreen.kt
@@ -1,6 +1,7 @@
 package com.example.bingetracker.pages
 
 import android.util.Log
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,9 +10,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -39,6 +43,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -208,14 +213,16 @@ fun ItemPopup(item: EntertainmentItem, onDismiss: () -> Unit, bingeModel: BingeM
     }
 
     if (showCreateDialog) {
+        val keyboardOpen = isKeyboardOpen()
+        val offsetY by animateDpAsState(targetValue = if (keyboardOpen) (-100).dp else 0.dp)
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            // Custom Dialog layout
             Card(
                 modifier = Modifier
                     .padding(16.dp)
+                    .offset(y = offsetY)
                     .fillMaxWidth()
                     .wrapContentHeight(),
                 shape = MaterialTheme.shapes.medium,
@@ -379,4 +386,12 @@ fun ItemPopup(item: EntertainmentItem, onDismiss: () -> Unit, bingeModel: BingeM
             }
         }
     }
+}
+
+@Composable
+fun isKeyboardOpen(): Boolean {
+    val ime = WindowInsets.ime
+    val density = LocalDensity.current
+    val imeBottom = ime.getBottom(density)
+    return imeBottom > 0
 }
